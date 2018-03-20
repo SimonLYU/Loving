d7520a2e1259db4bd6d88f198e629099d06b0c8d
吕旭明 <7964852@qq.com>
2018-03-20 18:09:30 +0800

    删除功能1的tableview

--- I❤U/I❤U/FunctionOne/FunctionOneHomeViewController.m
+++ I❤U/I❤U/FunctionOne/FunctionOneHomeViewController.m
@@ -9,36 +9,36 @@
 #import "FunctionOneHomeViewController.h"
 
 @interface FunctionOneHomeViewController ()<UITableViewDelegate,UITableViewDataSource>
-@property (weak, nonatomic) IBOutlet UITableView *tableView;
+//@property (weak, nonatomic) IBOutlet UITableView *tableView;
 @end
 
 @implementation FunctionOneHomeViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    [self setupData];
+//    [self setupData];
 }
 
 - (void)setupData{
-    [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"cell"];
-    self.tableView.delegate = self;
-    self.tableView.dataSource = self;
+//    [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"cell"];
+//    self.tableView.delegate = self;
+//    self.tableView.dataSource = self;
 }
 
 #pragma mark - UITableViewDataSource
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
-{
-    return 1;
-}
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
-{
-    return 10;
-}
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    UITableViewCell * cell = [tableView dequeueReusableCellWithIdentifier:@"cell"];
-    cell.textLabel.text = @"ceshi...";
-    return cell;
-}
+//- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+//{
+//    return 1;
+//}
+//- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+//{
+//    return 10;
+//}
+//- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+//{
+//    UITableViewCell * cell = [tableView dequeueReusableCellWithIdentifier:@"cell"];
+//    cell.textLabel.text = @"ceshi...";
+//    return cell;
+//}
 #pragma mark - UITableViewDelegate
 @end
