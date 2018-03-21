541a74df33e023577eb9b53e673b784f7f49ab22
吕旭明 <7964852@qq.com>
2018-03-21 19:48:14 +0800

    添加本地推送功能

--- I❤U/I❤U/AppDelegate.m
+++ I❤U/I❤U/AppDelegate.m
@@ -9,18 +9,71 @@
 #import "AppDelegate.h"
 #import "Log.h"
 
-@interface AppDelegate ()
+#ifdef NSFoundationVersionNumber_iOS_9_x_Max
+#import <UserNotifications/UserNotifications.h>
+#endif
+#define SYSTEM_VERSION [UIDevice currentDevice].systemVersion.floatValue
+
+@interface AppDelegate ()<UNUserNotificationCenterDelegate>
 
 @end
 
 @implementation AppDelegate
 
-
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     [Log info:NSStringFromClass(self.class) message:@"onInit"];
+    
+    if (SYSTEM_VERSION >= 10.0) {
+        //iOS10特有
+        UNUserNotificationCenter *center = [UNUserNotificationCenter currentNotificationCenter];
+        // 必须写代理，不然无法监听通知的接收与点击
+        center.delegate = self;
+        [center requestAuthorizationWithOptions:(UNAuthorizationOptionAlert | UNAuthorizationOptionBadge | UNAuthorizationOptionSound) completionHandler:^(BOOL granted, NSError * _Nullable error) {
+            if (granted) {
+                // 点击允许
+                NSLog(@"注册成功");
+                [center getNotificationSettingsWithCompletionHandler:^(UNNotificationSettings * _Nonnull settings) {
+                    NSLog(@"%@", settings);
+                }];
+            } else {
+                // 点击不允许
+                NSLog(@"注册失败");
+            }
+        }];
+    }else if ([[UIDevice currentDevice].systemVersion floatValue] >8.0){
+        //iOS8 - iOS10
+        [application registerUserNotificationSettings:[UIUserNotificationSettings settingsForTypes:UIUserNotificationTypeAlert | UIUserNotificationTypeSound | UIUserNotificationTypeBadge categories:nil]];
+        
+    }else if ([[UIDevice currentDevice].systemVersion floatValue] < 8.0) {
+        //iOS8系统以下
+        [application registerForRemoteNotificationTypes:UIRemoteNotificationTypeBadge | UIRemoteNotificationTypeAlert | UIRemoteNotificationTypeSound];
+    }
+    // 注册获得device Token
+    [[UIApplication sharedApplication] registerForRemoteNotifications];
+    
+    //LocalNotification :  每天中午12点发一条通知
+    UILocalNotification *local  = [[UILocalNotification alloc] init];
+    local.fireDate = [NSDate dateWithTimeIntervalSinceNow:20];
+//    local.fireDate = [NSDate dateWithTimeIntervalSince1970:4*60*60];//ps:since1970是从北京时间1970年1月1日早上8点开始算
+    local.soundName = UILocalNotificationDefaultSoundName;
+    local.alertBody = @"老婆大人,中午十二点到了,你家亲爱的又想你了哦~";
+    local.repeatInterval = kCFCalendarUnitDay;//循环通知的周期:每天
+    local.timeZone = [NSTimeZone defaultTimeZone];
+    local.applicationIconBadgeNumber = 0; //应用程序的右上角小数字
+    [[UIApplication sharedApplication] scheduleLocalNotification:local];
+    
     return YES;
 }
 
+// 获得Device Token
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
+    [Log info:NSStringFromClass(self.class) message:@"Device Token: %@", deviceToken];
+}
+// 获得Device Token失败
+- (void)application:(UIApplication *)application
+didFailToRegisterForRemoteNotificationsWithError:(NSError *)error {
+    [Log info:NSStringFromClass(self.class) message:@"Fail To Register For Remote Notifications With Error: %@", error];
+}
 
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
