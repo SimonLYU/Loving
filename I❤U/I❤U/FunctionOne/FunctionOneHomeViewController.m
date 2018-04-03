d91346562004e8c679c88330072f64b9b60f8ae2
吕旭明 <7964852@qq.com>
2018-04-03 17:16:24 +0800

    修改功能1

--- I❤U/I❤U/FunctionOne/FunctionOneHomeViewController.m
+++ I❤U/I❤U/FunctionOne/FunctionOneHomeViewController.m
@@ -8,9 +8,14 @@
 
 #import "FunctionOneHomeViewController.h"
 #import "BaseHeaders.h"
+#import "Masonry.h"
+#import "LOTAnimationView+Extension.h"
 
 @interface FunctionOneHomeViewController ()
+
 @property (weak, nonatomic) IBOutlet UILabel *detailLabel;
+@property (nonatomic, strong) LOTAnimationView *animationView;
+
 @end
 
 @implementation FunctionOneHomeViewController
@@ -23,9 +28,42 @@
 - (void)viewWillAppear:(BOOL)animated
 {
     [super viewWillAppear:animated];
-    
     [self setupData];
 }
+- (void)setupUI{
+    //navigationBar
+    [self.navigationController.navigationBar setHidden:YES];
+    //animation
+    if (!self.animationView) {
+        NSString * animationName = @"starrysky_full";
+        NSString * rootDir = [[[NSBundle mainBundle] bundlePath] stringByAppendingPathComponent:@"LOTLocalResource/RealTimeVoiceGame/Effect"];
+        self.animationView = [LOTAnimationView animationNamed:animationName rootDir:rootDir subDir:animationName];
+        [self.view insertSubview:self.animationView atIndex:0];
+        CGFloat expectWidth = [UIScreen mainScreen].bounds.size.height*self.animationView.frame.size.width / self.animationView.frame.size.height;
+        CGFloat expectHeight = [UIScreen mainScreen].bounds.size.width*self.animationView.frame.size.height / self.animationView.frame.size.width;
+        //screen full
+        if ([UIScreen mainScreen].bounds.size.height / self.animationView.frame.size.height > [UIScreen mainScreen].bounds.size.width / self.animationView.frame.size.width){
+            expectHeight = [UIScreen mainScreen].bounds.size.height;
+            expectWidth = expectHeight*self.animationView.frame.size.width / self.animationView.frame.size.height;
+        }else{
+            expectWidth = [UIScreen mainScreen].bounds.size.width;
+            expectHeight = expectWidth*self.animationView.frame.size.height / self.animationView.frame.size.width;
+        }
+        [self.animationView mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.center.mas_equalTo(self.view);
+            make.width.mas_equalTo(expectWidth);
+            make.height.mas_equalTo(expectHeight);
+        }];
+        self.animationView.loopAnimation = NO;
+        [self.animationView playWithCompletion:^(BOOL animationFinished) {
+            [Log info:NSStringFromClass(self.class) message:@"动画播放完毕:%@",animationName];
+        }];
+    }
+    //label
+    self.detailLabel.textColor = [UIColor whiteColor];
+    self.detailLabel.text = @"暂时还没有想要说的话哦~";
+}
+
 - (void)setupData{
     __weak typeof(self) wSelf = self;
     [[HttpUtil sharedInstance] getHttpRequestForPath:@"http://192.168.80.201:8080/user/22" paras:nil completion:^(NSData *result, NSURLResponse *response, NSError *error) {
@@ -43,9 +81,5 @@
             [Log info:NSStringFromClass(wSelf.class) message:@"result is nil!"];
         }
     }];
-}
-
-- (void)setupUI{
-    self.detailLabel.text = @"暂时还没有想要说的话哦~";
 }
 @end
--- I❤U/I❤U/FunctionTwo/FunctionTwoHomeViewController.m
+++ I❤U/I❤U/FunctionTwo/FunctionTwoHomeViewController.m
@@ -7,13 +7,9 @@
 //
 
 #import "FunctionTwoHomeViewController.h"
-#import "Masonry.h"
-#import "LOTAnimationView+Extension.h"
 #import "BaseHeaders.h"
 
 @interface FunctionTwoHomeViewController ()
-
-@property (nonatomic, strong) LOTAnimationView *animationView;
 
 @end
 
@@ -21,25 +17,5 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    [self setupUI];
 }
-
-- (void)setupUI{
-    NSString * animationName = @"background_full";
-    NSString * rootDir = [[[NSBundle mainBundle] bundlePath] stringByAppendingPathComponent:@"LOTLocalResource/RealTimeVoiceGame/Effect"];
-    self.animationView = [LOTAnimationView animationNamed:animationName rootDir:rootDir subDir:animationName];
-//    self.animationView.frame = self.view.bounds;
-    [self.view addSubview:self.animationView];
-    [self.animationView mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.top.mas_equalTo(self.view.mas_top);
-        make.left.mas_equalTo(self.view.mas_left);
-        make.right.mas_equalTo(self.view.mas_right);
-        make.bottom.mas_equalTo(self.view.mas_bottom);
-    }];
-    self.animationView.loopAnimation = NO;
-    [self.animationView playWithCompletion:^(BOOL animationFinished) {
-        [Log info:NSStringFromClass(self.class) message:@"动画播放完毕:%@",animationName];
-    }];
-}
-
 @end
