bde74b0f2831f866094a1b7a38e3e653928f0d64
吕旭明 <lvxuming@dafycredit.com>
2018-06-13 10:41:54 +0800

    添加appstore打包上架注释

--- I❤U/I❤U/BaseHeaders.h
+++ I❤U/I❤U/BaseHeaders.h
@@ -15,13 +15,23 @@
 #import <ReactiveCocoa/RACSignal.h>
 #import <ReactiveCocoa/RACTuple.h>
 
-//#import "BaseViewModel.h"
-//#import "BaseViewController.h"
-//#import "BaseNavigationController.h"
-//#import "BaseTabBarController.h"
 #import "Log.h"
 #import "HttpUtil.h"
 #import "UIUtil.h"
+/**
+ * 由于 iOS 编译的特殊性，为了方便开发者使用，我们将 i386 x86_64 armv7 arm64 几个平台都合并到了一起
+ * 所以使用动态库上传appstore时需要将i386 x86_64两个平台删除后，才能正常提交审核
+ * 在SDK当前路径下执行以下命令删除i386 x86_64两个平台
+ * bak文件是备份目录，上传appstore之后需要替换回bak目录下的SDK
+ */
+/*
+mkdir ./bak
+cp -r HyphenateLite.framework ./bak
+lipo HyphenateLite.framework/HyphenateLite -thin armv7 -output HyphenateLite_armv7
+lipo HyphenateLite.framework/HyphenateLite -thin arm64 -output HyphenateLite_arm64
+lipo -create HyphenateLite_armv7 HyphenateLite_arm64 -output HyphenateLite
+mv HyphenateLite HyphenateLite.framework/
+ */
 
 #define pixelWH (UIScreen.mainScreen.bounds.size.width * 0.1)
 
